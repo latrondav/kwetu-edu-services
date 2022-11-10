fc4b7483a0d2c744d69f9d1fd8bce17d95c5668a
latrondav <89303631+latrondav@users.noreply.github.com>
2022-11-10 16:25:40 +0300

    update

--- kwetuapp/urls.py
+++ kwetuapp/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.say_hello)
+    path('', views.home),
+    path('about/', views.about),
+    path('services/', views.services),
+    path('team/', views.team),
+    path('upcoming_events/', views.upcoming_events),
+    path('past_events/', views.past_events),
+    path('recorded_events/', views.recorded_events),
+    path('members/', views.members),
+    path('contact/', views.contact),
 ]
--- kwetuapp/views.py
+++ kwetuapp/views.py
@@ -2,5 +2,29 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def say_hello(request):
-    return render(request, 'hello.html', {'name': 'LATIM'})
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def team(request):
+    return render(request, 'team.html')
+
+def upcoming_events(request):
+    return render(request, 'events.html')
+
+def past_events(request):
+    return render(request, 'past_events.html')
+
+def recorded_events(request):
+    return render(request, 'recorded_events.html')
+
+def members(request):
+    return render(request, 'members.html')
+
+def contact(request):
+    return render(request, 'contact.html')
