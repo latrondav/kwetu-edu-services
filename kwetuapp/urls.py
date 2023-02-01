c4bffbe9e6d7e225a08be0b58764a759470040aa
latrondav <89303631+latrondav@users.noreply.github.com>
2023-02-01 16:18:25 +0300

    commit

--- kwetuapp/urls.py
+++ kwetuapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home),
@@ -17,4 +18,11 @@ urlpatterns = [
     path('recorded_events/', views.recorded_events),
     path('members/', views.members),
     path('contact/', views.contact),
+
+    #reset password path
+    path('resetpasswordform/', auth_views.PasswordResetView.as_view(template_name="password_mgt/pw_reset_form.html"), name= "password_reset_form"),
+    path('resetpassworddone/', auth_views.PasswordResetDoneView.as_view(template_name="password_mgt/pw_reset_done.html"), name= "password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="password_mgt/pw_reset_confirm.html"), name= "password_reset_confirm"),
+    path('resetpasswordcomplete/', auth_views.PasswordResetCompleteView.as_view(template_name="password_mgt/pw_reset_complete.html"), name= "password_reset_complete"),
+
 ]
