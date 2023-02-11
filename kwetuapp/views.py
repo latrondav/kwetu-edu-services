483e63c4c83ade6e6b4b494a8d74cd8dc5e47691
latrondav <89303631+latrondav@users.noreply.github.com>
2023-02-11 14:53:14 +0300

    progress

--- kwetuapp/views.py
+++ kwetuapp/views.py
@@ -371,15 +371,6 @@ def update_member(request):
         profile.position = position
         profile.save()
 
-        if profile.position == "Team Member":
-            user = User.objects.get(id=member_id)
-            user.is_staff = True
-            user.save()
-        else:
-            user = User.objects.get(id=member_id)
-            user.is_staff = False
-            user.save()
-
         messages.success(request, "Member Position Updated Successfully")
         return redirect('/members/')
     else:
